bd40d4beba211fc195b73353249a93bd52713b87
KidSea <594643219@qq.com>
2017-11-27 20:30:59 +0800

    initial project files

---
+++ app/src/main/java/com/example/yuxuehai/wallpager/base/BaseActivity.java
@@ -0,0 +1,54 @@
+package com.example.yuxuehai.wallpager.base;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuxuehai on 17-11-27.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private static final String TAG = BaseActivity.class.getName();
+
+    protected Context getcontext() {
+        return getApplicationContext();
+    }
+
+    protected Activity getActivityContext() {
+        return this;
+    }
+
+    protected abstract int requestLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        onBeforeSetContentView();
+
+        if(requestLayout() != 0) {
+            setContentView(requestLayout());
+        }
+
+        initView();
+        initData();
+        setupActionBar();
+    }
+
+
+    protected void onBeforeSetContentView(){
+
+    }
+
+    protected void setupActionBar(){
+
+    }
+}
---
+++ app/src/main/java/com/example/yuxuehai/wallpager/base/BaseFragment.java
@@ -0,0 +1,57 @@
+package com.example.yuxuehai.wallpager.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yuxuehai.wallpager.ui.MainActivity;
+
+
+/**
+ * Created by yuxuehai on 17-11-27.
+ */
+
+public abstract class BaseFragment extends Fragment{
+
+    protected MainActivity mActivity;
+    private View mMainView = null;
+
+    protected abstract View createView(LayoutInflater inflater, ViewGroup container,
+                                       Bundle savedInstanceState);
+
+    protected abstract void initView(View rootView);
+
+    protected abstract void initData();
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getActivity() instanceof MainActivity){
+            mActivity = (MainActivity)getActivity();
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        if (mMainView == null || mMainView.getParent() != null) {
+            mMainView = createView(inflater, container, savedInstanceState);
+            initView(mMainView);
+            initData();
+        }
+        return mMainView;
+    }
+
+
+    protected Intent getIntent(){
+        return mActivity.getIntent();
+    }
+}
